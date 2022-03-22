e87a46cd07f9ae29b5bf51e241eebe76ce423614
Stewart Lynch <slynch@createchsol.com>
2022-03-22 14:07:45 -0700

    Initial Commit

---
+++ TestRealmDifferentName/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestRealmDifferentName
+//
+//  Created by Stewart Lynch on 2022-03-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestRealmDifferentName/TestRealmDifferentNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestRealmDifferentNameApp.swift
+//  TestRealmDifferentName
+//
+//  Created by Stewart Lynch on 2022-03-22.
+//
+
+import SwiftUI
+
+@main
+struct TestRealmDifferentNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
